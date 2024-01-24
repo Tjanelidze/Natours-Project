073ae05063f7d70759a3ee9ee48785eeeda30d33
Tedo <tedojanelidze7@gmail.com>
2024-01-24 20:34:32 +0400

    created process env

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,7 +27,6 @@ exports.checkBody = (req, res, next) => {
 };
 
 exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
