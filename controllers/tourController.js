1ade243a95aeb5b477c48c7b475091a39dd7cf97
Tjanelidze <tedojanelidze7@gmail.com>
2024-02-01 10:04:38 +0100

    added error handling for updating

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -58,19 +58,14 @@ exports.createTour = catchAsync(async (req, res, next) => {
 });
 
 exports.updateTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    {
-      new: true,
-      runValidators: true,
-    },
-    (err) => {
-      if (err) {
-        return next(new AppError('No tour found with that ID', 404));
-      }
-    },
-  );
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(200).json({
     status: 'success',
