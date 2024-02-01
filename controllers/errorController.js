0477a615c98ca6fb3d2466df08171d4c4ad2f7cd
Tjanelidze <tedojanelidze7@gmail.com>
2024-02-01 09:45:59 +0100

    added error handling for duplicate creation of names

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,18 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+
+  return new AppError(message, 400);
+};
+
+const handeleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -34,6 +49,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = Object.create(err);
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handeleDuplicateFieldsDB(err);
+    sendErrorProd(error, res);
+    //sendErrorProd(error, res);
   }
 };
