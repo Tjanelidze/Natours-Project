9d348cc93c760d0786803e43f654d8fd2b025aca
Tjanelidze <tedojanelidze7@gmail.com>
2024-02-05 22:45:44 +0100

    getting token and check if its there

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,6 +64,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     return next(
       new AppError('You are not logged in! Please log in to get access.', 401),
     );
+
   // 2) Verification token
 
   // 3) Check if user still exists
