90313bc6d127f5b2c5365bb08588f74572011bb5
Tedo <tedojanelidze7@gmail.com>
2024-02-23 18:46:09 +0400

    implemented factory functions for update and create

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,4 @@
 const Review = require('../models/reviewModel');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getAllReviews = async (req, res, next) => {
@@ -17,19 +16,14 @@ exports.getAllReviews = async (req, res, next) => {
   });
 };
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
 
-  const newReview = await Review.create(req.body);
+  next();
+};
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview,
-    },
-  });
-});
-
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
