3b9c35ad6277c8565f4e0b94a72144ed0099bc8c
Tedo <tedojanelidze7@gmail.com>
2024-03-09 11:19:42 +0400

    implemented update of account

--- controllers/authController.js
+++ controllers/authController.js
@@ -76,7 +76,7 @@ exports.logout = (req, res) => {
 };
 
 exports.protect = catchAsync(async (req, res, next) => {
-  // 1) Getting token and check if it's there
+  // 1) Getting token and check of it's there
   let token;
   if (
     req.headers.authorization &&
@@ -87,24 +87,25 @@ exports.protect = catchAsync(async (req, res, next) => {
     token = req.cookies.jwt;
   }
 
-  if (!token)
+  if (!token) {
     return next(
       new AppError('You are not logged in! Please log in to get access.', 401),
     );
+  }
 
   // 2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3) Check if user still exists
   const currentUser = await User.findById(decoded.id);
-
-  if (!currentUser)
+  if (!currentUser) {
     return next(
       new AppError(
-        'The user belonging to this token does no longet exist.',
+        'The user belonging to this token does no longer exist.',
         401,
       ),
     );
+  }
 
   // 4) Check if user changed password after the token was issued
   if (currentUser.changedPasswordAfter(decoded.iat)) {
@@ -154,7 +155,8 @@ exports.restrictTo =
   (...roles) =>
   (req, res, next) => {
     // roles ['admin', 'lead-guide']. role='user'
-    if (!roles.includes(req.user.role)) {
+    console.log(roles.includes(req.user.role));
+    if (roles.includes(req.user.role)) {
       return next(
         new AppError('You do not have permission to perform this action', 403),
       );
--- controllers/userController.js
+++ controllers/userController.js
@@ -28,8 +28,9 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     );
   }
 
-  // 3) Filtered out unwanted fields names that are not allowed to be updated
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
