28630ccc21485cbf5031ee390775f4904a1843ad
Tedo <tedojanelidze7@gmail.com>
2024-03-09 17:55:49 +0400

    implemnted processing multiple images in tours

--- controllers/authController.js
+++ controllers/authController.js
@@ -155,8 +155,7 @@ exports.restrictTo =
   (...roles) =>
   (req, res, next) => {
     // roles ['admin', 'lead-guide']. role='user'
-    console.log(roles.includes(req.user.role));
-    if (roles.includes(req.user.role)) {
+    if (!roles.includes(req.user.role)) {
       return next(
         new AppError('You do not have permission to perform this action', 403),
       );
