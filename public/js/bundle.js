55aaf52730c3d074a56089fde2b032439c671a0f
Tedo <tedojanelidze7@gmail.com>
2024-03-09 16:08:10 +0400

    implemnted multer for uploading image

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12365,7 +12365,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36983" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43295" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
